8ea5bc86af082bb5b21606eac7317b6e309f38bf
dev-bilal-raza <success9262@gmail.com>
2025-12-31 00:08:33 +0500

    refactor class02.py to uncomment variable declarations and print statements for clarity

--- class02.py
+++ class02.py
@@ -1,5 +1,5 @@
-name: str = ""
-print(name)
+
+# print(name)
 
 # str
 # int
@@ -8,3 +8,21 @@ print(name)
 # tuple
 # dic
 # set
+
+details: str = '''I'm Bilal Raza. I'm student of DHA Suffa. I like to learn new things.
+I like to code'''
+
+# print(details)
+
+name: str = "abdullah"
+age: int = 12
+
+std_details = f"I'm {name}."
+print(std_details)
+
+user_details = f"""
+Name: {name}
+Age: {age}
+"""
+
+print(user_details)
